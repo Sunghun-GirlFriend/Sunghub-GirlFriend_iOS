f0668068a92f485884911f215ea5b8caa9b7dee3
JiHoonAHN <ahnjh1028@naver.com>
2023-01-15 00:59:45 +0900

    Make GodsaengTextfield

---
+++ Project/App/Sources/Core/DesignSystem/TextField/GodsaengTextfield.swift
@@ -0,0 +1,9 @@
+//
+//  GodsaengTextfielcd.swift
+//  Godsaeng-iOS
+//
+//  Created by Ji-hoon Ahn on 2023/01/15.
+//  Copyright © 2023 Sunghun-GirlFriend. All rights reserved.
+//
+
+import Foundation
