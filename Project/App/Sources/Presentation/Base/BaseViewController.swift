fba4063cb9d6dd9a9e02c67ae7bbebc6c3368007
JiHoonAHN <ahnjh1028@naver.com>
2023-01-14 18:07:46 +0900

    Make BaseViewController

--- Project/App/Sources/Presentation/Base/BaseViewController.swift
+++ Project/App/Sources/Presentation/Base/BaseViewController.swift
@@ -1,5 +1,57 @@
 import UIKit
+import ReactorKit
+import RxSwift
 
-internal class BaseViewController: UIViewController {
-    
+open class BaseViewController<T: Reactor>: UIViewController {
+    public let bounds = UIWindow().bounds
+    public var disposeBag = DisposeBag()
+
+    // MARK: - init
+    public init(reactor: T) {
+        super.init(nibName: nil, bundle: nil)
+        self.reactor = reactor
+    }
+    @available(*, unavailable)
+    required public init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setUp()
+        addView()
+        setLayout()
+    }
+
+    // MARK: - LifeCycle
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        setUp()
+        addView()
+        setLayout()
+    }
+    public override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        setLayoutSubviews()
+    }
+
+    // MARK: - Deinitalizer
+    deinit {
+        print("\(type(of: self)): \(#function)")
+    }
+
+    // MARK: - Method
+    open func setUp() {}
+    open func addView() {}
+    open func setLayout() {}
+    open func setLayoutSubviews() {}
+
+    // MARK: - Bind
+    open func bindView(reactor: T) {}
+    open func bindAction(reactor: T) {}
+    open func bindState(reactor: T) {}
+}
+
+extension BaseViewController: View {
+    public func bind(reactor: T) {
+        bindView(reactor: reactor)
+        bindState(reactor: reactor)
+        bindAction(reactor: reactor)
+    }
 }
