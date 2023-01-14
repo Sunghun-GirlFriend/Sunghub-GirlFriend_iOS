9f57daaefa08c24f5c6da411995b0fe1c106b2f4
JiHoonAHN <ahnjh1028@naver.com>
2023-01-15 08:35:05 +0900

    Update Profile

--- Project/App/Sources/Presentation/Rank/RankViewController.swift
+++ Project/App/Sources/Presentation/Rank/RankViewController.swift
@@ -2,4 +2,19 @@ import UIKit
 import ReactorKit
 import RxSwift
 
-final class RankViewController: BaseViewController<RankReactor> {}
+final class RankViewController: BaseViewController<RankReactor> {
+    private let image = UIImageView(image: UIImage(named: "RankPage"))
+    
+    override func addView() {
+        view.addSubViews(image)
+    }
+    
+    override func setLayout() {
+        image.snp.makeConstraints {
+            $0.top.equalTo(view.safeArea.top).offset(20)
+            $0.leading.trailing.equalToSuperview().inset(10)
+            $0.bottom.equalToSuperview()
+        }
+    }
+    
+}
