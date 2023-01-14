e832a7bdf8721caf09b7173d669aa0396ebd28e5
JiHoonAHN <ahnjh1028@naver.com>
2023-01-14 23:21:24 +0900

    Make Main

---
+++ Project/App/Sources/Presentation/Main/MainReactor.swift
@@ -0,0 +1,49 @@
+import Foundation
+import ReactorKit
+import RxSwift
+import RxRelay
+import RxFlow
+
+final class MainReactor: Reactor , Stepper{
+    
+    private let disposeBag: DisposeBag = .init()
+    var steps: PublishRelay<Step> = .init()
+
+    //MARK: - Reactor
+    enum Action {
+        // actiom cases
+    }
+    
+    enum Mutation {
+        // mutation cases
+    }
+    
+    struct State {
+        //state
+    }
+    
+    let initialState: State
+    
+    init() {
+        self.initialState = State()
+    }
+
+}
+
+//MARK: - Mutate
+extension MainReactor {
+    func mutate(action: Action) -> Observable<Mutation> {
+        // switch action {
+        // }
+    }
+}
+
+//MARK: - reduce
+extension MainReactor {
+    func reduce(state: State, mutation: Mutation) -> State {
+        var newState = state
+        // switch mutation {
+        // }
+        return newState
+    }
+}
---
+++ Project/App/Sources/Presentation/Main/MainViewController.swift
@@ -0,0 +1,6 @@
+import UIKit
+import ReactorKit
+import RxSwift
+
+final class MainViewController: BaseViewController<MainReactor>{
+}
